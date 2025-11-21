101c14318e5b0c423fe62a104d7624ebc891d65a
0050505ishan <isan91798@gmail.com>
2025-11-21 19:54:21 +0545

    commit

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -1,17 +1,9 @@
 import { Request, Response } from "express";
-import { z } from "zod";
-
-export const BookSchema = z.object({
-  id: z.string().min(1, { message: "Book Id is required" }),
-  title: z.string().min(1, { message: "Book title is required" }),
-  date: z.string().optional()
-});
-
-export type Book = z.infer<typeof BookSchema>; // Typescript type from Zod schema
-
-// DTO - Data Transfer Object
-export const CreateBookDTO = BookSchema.pick({ id: true, title: true }); // what client sends to server
-export type CreateBookDTO = z.infer<typeof CreateBookDTO>;
+import { CreateBookDTO } from "../dtos/book.dto";
+import { book } from "../repositories/book.repository";
+import { Book } from "../types/book.type";
+import { BookService } from "../services/book.service";
+import { error } from "console";
 
 // export type Book = {
 //   id: string;
@@ -19,45 +11,30 @@ export type CreateBookDTO = z.infer<typeof CreateBookDTO>;
 //   date?: string;
 // }
 
-export const book: Book[] = [
-  {
-        id: "B-1",
-        title: "1999",
-      },
-      {
-        id: "B-2",
-        title: "To Kill a Mockingbird",
-        date: "2015-12-10",
-      }
-]
+const bookService = new BookService();
 
 export class BookController {
   createBook = (req: Request, res: Response) => {
-    const validation = CreateBookDTO.safeParse(req.body);
+
+    try{
+      const validation = CreateBookDTO.safeParse(req.body);
     if(!validation.success){
         return res.status(400).json({errors: validation.error});
     }
-    const { id, title } = req.body // destructing
-    // const id: string = req.body.id;
-    // if (!id) {
-    //   return res.status(400).json({ message: "BookID is required" });
-    // }
-    // if (!title) {
-    //   return res.status(400).json({ message: "Book Title is required" });
-    // }
-    const checkBook = book.find(elem => elem.id == id);    
-    if (!checkBook) {
-      return res.status(400).json({ message: "Book ID already exists" });
-    }
-    const newBook: Book = { id, title };
-    // same as {id: id, title: title}, if key and variable name are same
-    book.push(newBook);
-    return res.status(201).json(newBook);
+    const { id, title } = validation.data;
+    const newBook: Book = bookService.createBook({id, title});
 
+    return res.status(201).json(newBook); 
+
+    }catch(err: Error | any){
+      return res.status(500).json({error: err.message ?? 'Internal Server Error'})
+    }
+
+    
   }
 
   getBooks = (req: Request, res: Response) => {
     const return_book: Book[] = book;
-    res.status(200).json(book);
+    res.status(200).json(return_book);
   };
 }
